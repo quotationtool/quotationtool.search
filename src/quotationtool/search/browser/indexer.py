70af9a103718d090b3e401aa6da678a1490243b5
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2012-01-27 21:33:33 +0100

    Fixed a bug in the dirty hack from some days ago

--- src/quotationtool/search/browser/indexer.py
+++ src/quotationtool/search/browser/indexer.py
@@ -2,21 +2,23 @@ import zope.component
 from z3c.pagelet.browser import BrowserPagelet
 from z3c.indexer.indexer import index
 from zope.intid.interfaces import IIntIds
+from z3c.indexer.interfaces import IIndex
 
 class IndexObjects(BrowserPagelet):
 
     count = 0
 
     def update(self):
+        # clear indices
+        for idx in zope.component.getAllUtilitiesRegisteredFor(IIndex, context=self.context):
+            idx.clear()
+        # index all registered objects
         super(IndexObjects, self).update()
         intids = zope.component.getUtility(
             IIntIds, context=self.context)
         for iid in intids:
-            try:
-                index(intids.getObject(iid))
-                self.count += 1
-            except Exception:
-                pass
+            index(intids.getObject(iid))
+            self.count += 1
 
     def render(self):
         return u"Indexed %d objects." % self.count
